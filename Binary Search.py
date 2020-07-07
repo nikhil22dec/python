bbc43800b036a83218664ac0462b3c1a0f3405f7
nikhil22dec <Nik#221287>
2020-07-07 15:52:18 -0700

    commit

--- Binary Search.py
+++ Binary Search.py
@@ -1,9 +1,9 @@
 checklist=[1,3,4,5,6,8,9,10]
 n=int(input('enter a number between 1 and 10'))
 start=0
-end=len(checklist)-1
+end=len(checklist)
 mid = int((end - start) / 2)
-while mid > start or mid < end or mid > 0:
+while mid > start and mid < end and mid > 0:
     mid = int((end - start) / 2)
     if checklist[mid]==n:
         print('Found the number at location: ',mid)
