afc37513c17e552cf91ed22e14232548cd472503
nikhil22dec <Nik#221287>
2020-07-20 23:02:29 -0700

    commit

--- File Overlap.py
+++ File Overlap.py
@@ -1,3 +1,5 @@
+https://www.practicepython.org/exercise/2014/12/14/23-file-overlap.html
+
 set1 =  set(open(r"C:\Users\jainnikh\Desktop\One.txt", 'r').read().split())
 set2 =  set(open(r"C:\Users\jainnikh\Desktop\the other.txt", 'r').read().split())
 set3=set1.intersection(set2)
